9c5e1c745fe67f1f91ea76ef92b923031a81c6fc
Kayner-Cueva <kayner1112@gmail.com>
2026-02-07 21:41:05 -0500

    chore: adapt port for render

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,7 @@
 import 'dotenv/config';
 import app from './app';
 
-const PORT = process.env.API_PORT
-  ? Number(process.env.API_PORT)
-  : 4000;
+const PORT = process.env.PORT || process.env.API_PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
